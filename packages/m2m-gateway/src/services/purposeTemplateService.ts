b195ce2afc949c98b293ca7af160ea01a1b22403
Davide Giunzioni <giunzioni.dp@gmail.com>
2025-11-20 15:51:27 +0100

    feature: first version PUT /purposeTemplates/:purposeTemplateId/riskAnalysis process + m2m (PIN-8272)

--- packages/m2m-gateway/src/services/purposeTemplateService.ts
+++ packages/m2m-gateway/src/services/purposeTemplateService.ts
@@ -18,6 +18,7 @@ import {
   toGetPurposeTemplatesApiQueryParams,
   toM2MGatewayApiPurposeTemplate,
   toM2MGatewayApiRiskAnalysisTemplateAnnotationDocument,
+  toPurposeTemplateApiRiskAnalysisFormTemplateSeed,
 } from "../api/purposeTemplateApiConverter.js";
 import { toM2MGatewayApiEService } from "../api/eserviceApiConverter.js";
 import { toM2MGatewayApiRiskAnalysisFormTemplate } from "../api/riskAnalysisFormTemplateApiConverter.js";
@@ -323,6 +324,30 @@ export function purposeTemplateServiceBuilder(
       );
 
       return toM2MGatewayApiPurposeTemplate(data);
+    },
+    async updatePurposeTemplateRiskAnalysis(
+      purposeTemplateId: PurposeTemplateId,
+      riskAnalysisFormSeed: m2mGatewayApi.RiskAnalysisFormTemplateSeed,
+      { headers, logger }: WithLogger<M2MGatewayAppContext>
+    ): Promise<m2mGatewayApi.RiskAnalysisFormTemplate> {
+      logger.info(
+        `Updating risk analysis form for purpose template ${purposeTemplateId}`
+      );
+
+      const { data: riskAnalysisForm } =
+        await clients.purposeTemplateProcessClient.updatePurposeTemplateRiskAnalysis(
+          toPurposeTemplateApiRiskAnalysisFormTemplateSeed(
+            riskAnalysisFormSeed
+          ),
+          {
+            params: {
+              purposeTemplateId,
+            },
+            headers,
+          }
+        );
+
+      return toM2MGatewayApiRiskAnalysisFormTemplate(riskAnalysisForm);
     },
   };
 }
