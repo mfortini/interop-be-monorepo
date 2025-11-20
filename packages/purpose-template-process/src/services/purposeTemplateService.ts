4c80e9fc85294a5c2b69e09323c48be9485fee98
Davide Giunzioni <giunzioni.dp@gmail.com>
2025-11-20 17:38:28 +0100

    feature: purpose template process tests

--- packages/purpose-template-process/src/services/purposeTemplateService.ts
+++ packages/purpose-template-process/src/services/purposeTemplateService.ts
@@ -1737,7 +1737,7 @@ export function purposeTemplateServiceBuilder(
       };
 
       await cleanupAnnotationDocsForRemovedAnswers(
-        purposeTemplateToApiPurposeTemplateSeed(purposeTemplate.data),
+        purposeTemplateToApiPurposeTemplateSeed(updatedPurposeTemplate),
         purposeTemplate.data,
         fileManager,
         logger
