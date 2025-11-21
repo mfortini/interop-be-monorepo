90e28ea8d02ded96224231a55f481fea7670ba57
Davide Giunzioni <giunzioni.dp@gmail.com>
2025-11-21 09:53:22 +0100

    feature: m2m tests

--- packages/m2m-gateway/src/services/purposeTemplateService.ts
+++ packages/m2m-gateway/src/services/purposeTemplateService.ts
@@ -23,6 +23,10 @@ import {
 import { toM2MGatewayApiEService } from "../api/eserviceApiConverter.js";
 import { toM2MGatewayApiRiskAnalysisFormTemplate } from "../api/riskAnalysisFormTemplateApiConverter.js";
 import { purposeTemplateRiskAnalysisFormNotFound } from "../model/errors.js";
+import {
+  pollResourceWithMetadata,
+  isPolledVersionAtLeastMetadataTargetVersion,
+} from "../utils/polling.js";
 
 export type PurposeTemplateService = ReturnType<
   typeof purposeTemplateServiceBuilder
@@ -334,7 +338,7 @@ export function purposeTemplateServiceBuilder(
         `Updating risk analysis form for purpose template ${purposeTemplateId}`
       );
 
-      const { data: riskAnalysisForm } =
+      const { data: riskAnalysisForm, metadata } =
         await clients.purposeTemplateProcessClient.updatePurposeTemplateRiskAnalysis(
           toPurposeTemplateApiRiskAnalysisFormTemplateSeed(
             riskAnalysisFormSeed
@@ -346,6 +350,8 @@ export function purposeTemplateServiceBuilder(
             headers,
           }
         );
+
+      await pollPurposeTemplateById(purposeTemplateId, metadata, headers);
 
       return toM2MGatewayApiRiskAnalysisFormTemplate(riskAnalysisForm);
     },
